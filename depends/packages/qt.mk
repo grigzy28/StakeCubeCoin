c127723afa05332a49d6d2942a9563f85a1a60fc
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2024-12-07 22:15:18 -0600

    Update qt.mk

--- depends/packages/qt.mk
+++ depends/packages/qt.mk
@@ -1,9 +1,9 @@
 package=qt
-$(package)_version=5.15.11
+$(package)_version=5.15.15
 $(package)_download_path=https://download.qt.io/official_releases/qt/5.15/$($(package)_version)/submodules
 $(package)_suffix=everywhere-opensource-src-$($(package)_version).tar.xz
 $(package)_file_name=qtbase-$($(package)_suffix)
-$(package)_sha256_hash=425ad301acd91ca66c10c0dabee0704e2d0cd2801a6b670115800cbb95f84846
+$(package)_sha256_hash=e5f941fecf694ecba97c550b45b0634e552166cc6c815bcfdc481edd62796ba1
 $(package)_linux_dependencies=freetype fontconfig libxcb libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
 $(package)_qt_libs=corelib network widgets gui plugins testlib
 $(package)_linguist_tools = lrelease lupdate lconvert
@@ -29,10 +29,10 @@ $(package)_patches += memory_resource.patch
 build_TAR = tar
 
 $(package)_qttranslations_file_name=qttranslations-$($(package)_suffix)
-$(package)_qttranslations_sha256_hash=a31785948c640b7c66d9fe2db4993728ca07f64e41c560b3625ad191b276ff20
+$(package)_qttranslations_sha256_hash=33E077EA098C86AD5616E5AE11458484BA846DB8E2F4524C0A4185C84ED6A806
 
 $(package)_qttools_file_name=qttools-$($(package)_suffix)
-$(package)_qttools_sha256_hash=7cd847ae6ff09416df617136eadcaf0eb98e3bc9b89979219a3ea8111fb8d339
+$(package)_qttools_sha256_hash=71946704c6bd6c925910288b97dfcc2e357d4a28e22c8651a5813aae4f238028
 
 $(package)_extra_sources  = $($(package)_qttranslations_file_name)
 $(package)_extra_sources += $($(package)_qttools_file_name)
