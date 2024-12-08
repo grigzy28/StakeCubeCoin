a8367af4ec82d1770c587c740f13b1bff4a6d073
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2024-12-08 14:25:54 -0600

    Update dbus.mk

--- depends/packages/dbus.mk
+++ depends/packages/dbus.mk
@@ -1,7 +1,7 @@
 package=dbus
 $(package)_version=1.15.90
 $(package)_download_path=https://dbus.freedesktop.org/releases/dbus
-$(package)_file_name=$(package)-$($(package)_version).tar.gz
+$(package)_file_name=$(package)-$($(package)_version).tar.xz
 $(package)_sha256_hash=54136AA36411F370A22D3C7D866E12EDEA4E5B0943A1D96DFE8A51E473023260
 $(package)_dependencies=expat
 
