1582834686223907bc9f48d4f46e5d6711b69446
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2024-12-08 13:58:20 -0600

    Update libxcb.mk

--- depends/packages/libxcb.mk
+++ depends/packages/libxcb.mk
@@ -1,8 +1,8 @@
 package=libxcb
-$(package)_version=1.14
+$(package)_version=1.17
 $(package)_download_path=https://xcb.freedesktop.org/dist
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=a55ed6db98d43469801262d81dc2572ed124edc3db31059d4e9916eb9f844c34
+$(package)_sha256_hash=599EBF9996710FEA71622E6E184F3A8AD5B43D0E5FA8C4E407123C88A59A6D55
 $(package)_dependencies=xcb_proto libXau
 $(package)_patches = remove_pthread_stubs.patch
 
