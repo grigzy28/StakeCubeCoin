0f75a057277c276eb02d4d229247e2091d577b3e
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2024-12-08 14:40:39 -0600

    Update dbus.mk

--- depends/packages/dbus.mk
+++ depends/packages/dbus.mk
@@ -1,42 +1,17 @@
 package=dbus
-$(package)_version=1.15.90
+$(package)_version=1.15.0
 $(package)_download_path=https://dbus.freedesktop.org/releases/dbus
 $(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_sha256_hash=54136AA36411F370A22D3C7D866E12EDEA4E5B0943A1D96DFE8A51E473023260
-$(package)_dependencies=expat cmake
+$(package)_sha256_hash=5073C8CB9AD20226647BB38F4965182B762A6E1F595CCDC8E59411014BFD640A
+$(package)_dependencies=expat
+
 define $(package)_set_vars
-  $(package)_config_opts=-DCMAKE_INSTALL_PREFIX=$(host_prefix)
-  $(package)_config_opts+= -DCMAKE_PREFIX_PATH=$(host_prefix)
-  $(package)_config_opts+= -DSTLIB=ON -DSHLIB=OFF -DSTBIN=OFF
-  $(package)_config_opts_linux=-DOPSYS=LINUX -DCMAKE_SYSTEM_NAME=Linux
-  $(package)_config_opts_darwin=-DOPSYS=MACOSX -DCMAKE_SYSTEM_NAME=Darwin
-  $(package)_config_opts_mingw32=-DOPSYS=WINDOWS -DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS=""
-  $(package)_config_opts_i686+= -DWSIZE=32
-  $(package)_config_opts_x86_64+= -DWSIZE=64
-  $(package)_config_opts_arm+= -DWSIZE=32
-  $(package)_config_opts_armv7l+= -DWSIZE=32
-  $(package)_config_opts_debug=-DDEBUG=ON -DCMAKE_BUILD_TYPE=Debug
-
-  ifneq ($(darwin_native_toolchain),)
-    $(package)_config_opts_darwin+= -DCMAKE_AR="$(host_prefix)/native/bin/$($(package)_darwin_triplet)-ar"
-    $(package)_config_opts_darwin+= -DCMAKE_LINKER="$(host_prefix)/native/bin/$($(package)_darwin_triplet)-ld"
-    $(package)_config_opts_darwin+= -DCMAKE_RANLIB="$(host_prefix)/native/bin/$($(package)_darwin_triplet)-ranlib"
-  endif
-
-  $(package)_cppflags+=-UBLSALLOC_SODIUM
-
+  $(package)_config_opts=--disable-tests --disable-doxygen-docs --disable-xml-docs --disable-static --without-x
 endef
-#  $(package)_config_opts=--disable-tests --disable-doxygen-docs --disable-xml-docs --disable-static --without-x
 
 define $(package)_config_cmds
-  export CC="$($(package)_cc)" && \
-  export CXX="$($(package)_cxx)" && \
-  export CFLAGS="$($(package)_cflags) $($(package)_cppflags)" && \
-  export CXXFLAGS="$($(package)_cxxflags) $($(package)_cppflags)" && \
-  export LDFLAGS="$($(package)_ldflags)" && \
-  $(host_prefix)/bin/cmake ../ $($(package)_config_opts)
+  $($(package)_autoconf)
 endef
-#  $($(package)_autoconf)
 
 define $(package)_build_cmds
   $(MAKE) -C dbus libdbus-1.la
