9a88490a25cd4bbec33c5da0fc5f54c108d1ac32
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2024-12-12 20:17:10 -0600

    combined updates
    qt 5.15.14 and other updates

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -1876,4 +1876,13 @@ int TextWidth(const QFontMetrics& fm, const QString& text)
 #endif
 }
 
+QDateTime StartOfDay(const QDate& date)
+{
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
+    return date.startOfDay();
+#else
+    return QDateTime(date);
+#endif
+}
+
 } // namespace GUIUtil
