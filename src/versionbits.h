a6c19e9b5056d205318912fff3de99abb8adc556
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-14 14:52:35 -0600

    Update versionbits.h

--- src/versionbits.h
+++ src/versionbits.h
@@ -80,8 +80,8 @@ class VersionBitsCache
 {
 private:
     Mutex m_mutex;
-    ThresholdConditionCache m_caches[Consensus::MAX_VERSION_BITS_DEPLOYMENTS] GUARDED_BY(m_mutex);
-    ThresholdConditionCache caches[Consensus::MAX_VERSION_BITS_DEPLOYMENTS];
+//    ThresholdConditionCache m_caches[Consensus::MAX_VERSION_BITS_DEPLOYMENTS] GUARDED_BY(m_mutex);
+    ThresholdConditionCache caches[Consensus::MAX_VERSION_BITS_DEPLOYMENTS] GUARDED_BY(m_mutex);
 
 public:
     /** Get the numerical statistics for a given deployment for the signalling period that includes pindex.
