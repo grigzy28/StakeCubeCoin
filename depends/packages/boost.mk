ef00b1bc6551ff50499ab0c1d8f3af1aec775211
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-13 22:12:43 -0600

    Update boost.mk

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,6 +1,6 @@
 package=boost
 $(package)_version=1_86_0
-$(package)_download_path=https://boostorg.jfrog.io/artifactory/main/release/$(subst _,.,$($(package)_version))/source/
+$(package)_download_path=https://archives.boost.io/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
 $(package)_sha256_hash=1bed88e40401b2cb7a1f76d4bab499e352fa4d0c5f31c0dbae64e24d34d7513b
 #4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402
