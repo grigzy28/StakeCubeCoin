eab04a20a16f57e25cd547f3677aba10529e4e4c
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-13 20:42:42 -0600

    Update env_posix.cc

--- src/leveldb/util/env_posix.cc
+++ src/leveldb/util/env_posix.cc
@@ -837,7 +837,7 @@ class SingletonEnv {
  public:
   SingletonEnv() {
 #if !defined(NDEBUG)
-    env_initialized_.store(true, std::memory_order::memory_order_relaxed);
+    env_initialized_.store(true, std::memory_order_relaxed);
 #endif  // !defined(NDEBUG)
     static_assert(sizeof(env_storage_) >= sizeof(EnvType),
                   "env_storage_ will not fit the Env");
