dfab76c44e888452ded05f511eeedb25ab02f3a9
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-13 19:18:12 -0600

    Update blockchain.cpp

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -28,6 +28,7 @@
 #include <txmempool.h>
 #include <util/strencodings.h>
 #include <util/validation.h>
+#include <util/check.h>
 #include <util/system.h>
 #include <validation.h>
 #include <validationinterface.h>
