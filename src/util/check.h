362a30da725fd61e79534e31793671c0ed30edaf
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-13 19:06:11 -0600

    Update check.h

--- src/util/check.h
+++ src/util/check.h
@@ -1,38 +1,72 @@
-// Copyright (c) 2019-2020 The Bitcoin Core developers
+// Copyright (c) 2019-2022 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_UTIL_CHECK_H
 #define BITCOIN_UTIL_CHECK_H
 
-#if defined(HAVE_CONFIG_H)
-#include <config/bitcoin-config.h>
-#endif
-
 #include <attributes.h>
-#include <tinyformat.h>
 
+#include <cassert> // IWYU pragma: export
 #include <stdexcept>
+#include <string>
+#include <string_view>
+#include <utility>
+
+constexpr bool G_FUZZING{
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+    true
+#else
+    false
+#endif
+};
+
+std::string StrFormatInternalBug(std::string_view msg, std::string_view file, int line, std::string_view func);
 
 class NonFatalCheckError : public std::runtime_error
 {
-    using std::runtime_error::runtime_error;
+public:
+    NonFatalCheckError(std::string_view msg, std::string_view file, int line, std::string_view func);
 };
-
-#define format_internal_error(msg, file, line, func, report)                                    \
-    strprintf("Internal bug detected: \"%s\"\n%s:%d (%s)\nPlease report this issue here: %s\n", \
-              msg, file, line, func, report)
 
 /** Helper for CHECK_NONFATAL() */
 template <typename T>
 T&& inline_check_non_fatal(LIFETIMEBOUND T&& val, const char* file, int line, const char* func, const char* assertion)
 {
-    if (!(val)) {
-        throw NonFatalCheckError(
-            format_internal_error(assertion, file, line, func, PACKAGE_BUGREPORT));
+    if (!val) {
+        throw NonFatalCheckError{assertion, file, line, func};
     }
     return std::forward<T>(val);
 }
+
+#if defined(NDEBUG)
+#error "Cannot compile without assertions!"
+#endif
+
+/** Helper for Assert() */
+void assertion_fail(std::string_view file, int line, std::string_view func, std::string_view assertion);
+
+/** Helper for Assert()/Assume() */
+template <bool IS_ASSERT, typename T>
+constexpr T&& inline_assertion_check(LIFETIMEBOUND T&& val, [[maybe_unused]] const char* file, [[maybe_unused]] int line, [[maybe_unused]] const char* func, [[maybe_unused]] const char* assertion)
+{
+    if constexpr (IS_ASSERT
+#ifdef ABORT_ON_FAILED_ASSUME
+                  || true
+#endif
+    ) {
+        if (!val) {
+            assertion_fail(file, line, func, assertion);
+        }
+    }
+    return std::forward<T>(val);
+}
+
+
+// All macros may use __func__ inside a lambda, so put them under nolint.
+// NOLINTBEGIN(bugprone-lambda-function-name)
+
+#define STR_INTERNAL_BUG(msg) StrFormatInternalBug((msg), __FILE__, __LINE__, __func__)
 
 /**
  * Identity function. Throw a NonFatalCheckError when the condition evaluates to false
@@ -47,29 +81,6 @@ T&& inline_check_non_fatal(LIFETIMEBOUND T&& val, const char* file, int line, co
  */
 #define CHECK_NONFATAL(condition) \
     inline_check_non_fatal(condition, __FILE__, __LINE__, __func__, #condition)
-
-#if defined(NDEBUG)
-#error "Cannot compile without assertions!"
-#endif
-
-/** Helper for Assert() */
-void assertion_fail(const char* file, int line, const char* func, const char* assertion);
-
-/** Helper for Assert()/Assume() */
-template <bool IS_ASSERT, typename T>
-T&& inline_assertion_check(LIFETIMEBOUND T&& val, [[maybe_unused]] const char* file, [[maybe_unused]] int line, [[maybe_unused]] const char* func, [[maybe_unused]] const char* assertion)
-{
-    if constexpr (IS_ASSERT
-#ifdef ABORT_ON_FAILED_ASSUME
-                  || true
-#endif
-    ) {
-        if (!val) {
-            assertion_fail(file, line, func, assertion);
-        }
-    }
-    return std::forward<T>(val);
-}
 
 /** Identity function. Abort if the value compares equal to zero */
 #define Assert(val) inline_assertion_check<true>(val, __FILE__, __LINE__, __func__, #val)
@@ -88,11 +99,11 @@ T&& inline_assertion_check(LIFETIMEBOUND T&& val, [[maybe_unused]] const char* f
 
 /**
  * NONFATAL_UNREACHABLE() is a macro that is used to mark unreachable code. It throws a NonFatalCheckError.
- * This is used to mark code that is not yet implemented or is not yet reachable.
  */
 #define NONFATAL_UNREACHABLE()                                        \
     throw NonFatalCheckError(                                         \
-        format_internal_error("Unreachable code reached (non-fatal)", \
-                              __FILE__, __LINE__, __func__, PACKAGE_BUGREPORT))
+        "Unreachable code reached (non-fatal)", __FILE__, __LINE__, __func__)
+
+// NOLINTEND(bugprone-lambda-function-name)
 
 #endif // BITCOIN_UTIL_CHECK_H
