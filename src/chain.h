c24ced2dd3a734aa4247fc5ffe7fea75a81a94ac
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-14 21:36:24 -0600

    Revert "Update chain.h"
    This reverts commit 02615aeaa1e195ea87803a111291ed8d48aa7cd7.

--- src/chain.h
+++ src/chain.h
@@ -454,8 +454,6 @@ public:
     CChain(const CChain&) = delete;
     CChain& operator=(const CChain&) = delete;
 
-    CChain& ActiveChain() const EXCLUSIVE_LOCKS_REQUIRED(GetMutex()) { return ActiveChainstate().m_chain; }
-
     /** Returns the index entry for the genesis block of this chain, or nullptr if none. */
     CBlockIndex *Genesis() const {
         return vChain.size() > 0 ? vChain[0] : nullptr;
