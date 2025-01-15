44e4cfebecd2c2c5bce4add96ec986890d7cd484
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-14 21:18:26 -0600

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -1148,11 +1148,14 @@ bool CChainState::IsInitialBlockDownload() const
         return false;
     if (fImporting || fReindex)
         return true;
-    if (m_chain.Tip() == nullptr)
+
+    CChain& chain{ActiveChain()};
+
+    if (chain.Tip() == nullptr)
         return true;
-    if (m_chain.Tip()->nChainWork < nMinimumChainWork)
+    if (chain.Tip()->nChainWork < nMinimumChainWork)
         return true;
-    if (m_chain.Tip()->GetBlockTime() < (GetTime() - nMaxTipAge))
+    if (chain.Tip()->GetBlockTime() < (GetTime() - nMaxTipAge))
         return true;
     LogPrintf("Leaving InitialBlockDownload (latching to false)\n");
     m_cached_finished_ibd.store(true, std::memory_order_relaxed);
