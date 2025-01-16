fb1f2455b5f0235cb0087f1301e0805af48ae01b
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-16 11:22:31 -0600

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -2590,8 +2590,11 @@ void static UpdateTip(const CBlockIndex *pindexNew, const CChainParams& chainPar
         const CBlockIndex* pindex = pindexNew;
         for (int bit = 0; bit < VERSIONBITS_NUM_BITS; bit++) {
 
+			WarningBitsConditionChecker checker(bit);
+
             if (!preloadedchain) ThresholdState state = PreLoadCacheBits(pindex, warningcache[bit]);
 
+//
 			WarningBitsConditionChecker checker(bit);
             ThresholdState state = checker.GetStateFor(pindex, chainParams.GetConsensus(), warningcache[bit]);
             if (state == ThresholdState::ACTIVE || state == ThresholdState::LOCKED_IN) {
