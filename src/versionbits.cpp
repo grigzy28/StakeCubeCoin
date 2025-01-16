c2fd3246e1e59e0e732eee5bf1198757573c841e
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-16 16:17:08 -0600

    Update versionbits.cpp

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -133,13 +133,13 @@ ThresholdState AbstractThresholdConditionChecker::GetStateForBuildCache(const CB
 	int maxtip = pindexPrev->nHeight;
 
     while (cache.count(pindexPrev) == 0) {
-        if (pindexPrev == nullptr) {
-            // The genesis block is by definition defined.
-            cache[pindexPrev] = ThresholdState::DEFINED;
-            break;
-        }
+//        if (pindexPrev == nullptr) {
+//            // The genesis block is by definition defined.
+//            cache[pindexPrev] = ThresholdState::DEFINED;
+//            break;
+//        }
         vToCompute.push_back(pindexPrev);
-        pindexPrev = pindexPrev->GetAncestor(pindexPrev->nHeight);
+        pindexPrev = pindexPrev->GetAncestor(pindexPrev->nHeight) - 1;
     }
 
     // At this point, cache[pindexPrev] is known
