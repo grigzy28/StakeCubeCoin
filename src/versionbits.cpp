7cfa611d40fba5b8265415cb2122d0835c78a396
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-01-16 21:56:39 -0600

    Update versionbits.cpp

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -160,7 +160,7 @@ ThresholdState AbstractThresholdConditionChecker::GetStateForBuildCache(const CB
         vToCompute.pop_back();
 		counter = counter + 1;
 
-		LogPrint(BCLog::BENCHMARK, "Second Height: %s - Counter: %s - Bit: %s\n", pindexPrev->nHeight, counter, bit);
+		LogPrint(BCLog::BENCHMARK, "Second Height: %s - Counter: %s - Bit: %s\n", pindexPrev->nHeight, counter, bitIn);
 
 		if (pindexPrev->nVersion == bitIn) cache[pindexPrev] = state = stateNext;
 
