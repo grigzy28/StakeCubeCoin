04ca7e5fd6aaaf3bea34baf78afdfb6c6557984c
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-08 16:31:05 -0600

    Update native_clang.mk

--- depends/packages/native_clang.mk
+++ depends/packages/native_clang.mk
@@ -1,14 +1,14 @@
 package=native_clang
-$(package)_version=10.0.1
+$(package)_version=17.0.2
 $(package)_download_path=https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(package)_version)
 ifneq (,$(findstring aarch64,$(BUILD)))
 $(package)_download_file=clang+llvm-$($(package)_version)-aarch64-linux-gnu.tar.xz
 $(package)_file_name=clang+llvm-$($(package)_version)-aarch64-linux-gnu.tar.xz
-$(package)_sha256_hash=90dc69a4758ca15cd0ffa45d07fbf5bf4309d47d2c7745a9f0735ecffde9c31f
+$(package)_sha256_hash=A73D9326E5D756E3937DF6A9F621664D76403B59119F741901106B387E53A6AE
 else
-$(package)_download_file=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+$(package)_download_file=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-22.04.tar.xz
 $(package)_file_name=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-$(package)_sha256_hash=48b83ef827ac2c213d5b64f5ad7ed082c8bcb712b46644e0dc5045c6f462c231
+$(package)_sha256_hash=DF297DF804766F8FB18F10A188AF78E55D82BB8881751408C2FA694CA19163A8
 endif
 
 define $(package)_preprocess_cmds
