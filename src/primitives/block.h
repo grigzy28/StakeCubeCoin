dd52640226d947b4a91406d3108091e265cf2721
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-08 13:56:56 -0600

    Revert "Update block.h"
    This reverts commit b4a874cd1307710352a44099318477acba1e3f36.

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -11,9 +11,8 @@
 #include "crypto/progpow.h"
 #include <serialize.h>
 #include <uint256.h>
-#include <chainparams.h>
 
-//extern uint32_t nPPSwitchTime;
+extern uint32_t nPPSwitchTime;
 
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
