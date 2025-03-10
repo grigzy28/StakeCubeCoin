e59bc8340e5b50d38eb4a122a183ccec835dc3a3
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-09 21:24:49 -0500

    Update cmake.mk

--- depends/packages/cmake.mk
+++ depends/packages/cmake.mk
@@ -1,8 +1,8 @@
 package=cmake
-$(package)_version=3.22.2
-$(package)_download_path=https://cmake.org/files/v3.22/
+$(package)_version=3.31.6
+$(package)_download_path=https://cmake.org/files/v3.31/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=3c1c478b9650b107d452c5bd545c72e2fad4e37c09b89a1984b9a2f46df6aced
+$(package)_sha256_hash=653427F0F5014750AAFFF22727FB2AA60C6C732CA91808CFB78CE22DDD9E55F0
 
 define $(package)_config_cmds
   export CC="" && \
