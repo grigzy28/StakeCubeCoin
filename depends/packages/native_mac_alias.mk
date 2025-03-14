75b996d931e4de241945003beceec989ccf03d24
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-14 10:48:33 -0500

    Update native_mac_alias.mk

--- depends/packages/native_mac_alias.mk
+++ depends/packages/native_mac_alias.mk
@@ -1,8 +1,8 @@
 package=native_mac_alias
-$(package)_version=bb20cd5bbc51248de15473da585d45eb83ca8217
+$(package)_version=2.2.0
 $(package)_download_path=https://github.com/al45tair/mac_alias/archive/
-$(package)_file_name=$($(package)_version).tar.gz
-$(package)_sha256_hash=521D7D4EAA0E8AEEDBE14FB21AAAF7A2973C55793D97BDFF73A03AEEC995F7A7
+$(package)_file_name=v$($(package)_version).tar.gz
+$(package)_sha256_hash=421E6D7586D1F155C7DB3E7DA01CA0DACC9649A509A253AD7077B70174426499
 $(package)_install_libdir=$(build_prefix)/lib/python3/dist-packages
 
 define $(package)_build_cmds
