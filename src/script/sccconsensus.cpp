8b7cfc67f3114ec8870d4ff114e6be81f3fbe719
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-14 11:48:41 -0500

    Update sccconsensus.cpp

--- src/script/sccconsensus.cpp
+++ src/script/sccconsensus.cpp
@@ -4,6 +4,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <script/sccconsensus.h>
+#include <consensus/params.h>
 
 #include <primitives/transaction.h>
 #include <pubkey.h>
