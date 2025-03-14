2fa46438f84e2b12836508af65b68bf5a25637dc
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-14 14:08:51 -0500

    Update boost.mk

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -1,5 +1,5 @@
 package=boost
-$(package)_version=1_81_0
+$(package)_version=1.81.0
 $(package)_download_path=https://archives.boost.io/release/$(subst _,.,$($(package)_version))/source/
 $(package)_file_name=boost_$($(package)_version).tar.bz2
 $(package)_sha256_hash=205666dea9f6a7cfed87c7a6dfbeb52a2c1b9de55712c9c1a87735d7181452b6
