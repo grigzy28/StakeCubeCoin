912cc530f463aea3720d56253bcfa691d7214f2b
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-15 00:15:28 -0500

    update gmp

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,13 +1,19 @@
 package=gmp
-$(package)_version=6.2.1
-$(package)_download_path=https://gmplib.org/download/gmp
+$(package)_version=6.3.0
+$(package)_download_path=https://ftp.gnu.org/gnu/gmp
 $(package)_file_name=gmp-$($(package)_version).tar.bz2
-$(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
+$(package)_sha256_hash=ac28211a7cfb609bae2e2c8d6058d66c8fe96434f740cf6fe2e47b000d1c20cb
+$(package)_patches=include_ldflags_in_configure.patch
 
 define $(package)_set_vars
-$(package)_config_opts+=--enable-cxx --enable-fat --with-pic --disable-shared
-$(package)_cflags_armv7l_linux+=-march=armv7-a
-$(package)_cflags_aarch64_darwin+=-march=armv8-a
+$(package)_config_opts += --disable-shared --enable-cxx --enable-fat
+$(package)_cflags_aarch64 += -march=armv8-a
+$(package)_cflags_armv7l += -march=armv7-a
+$(package)_cflags_x86_64 += -march=x86-64
+endef
+
+define $(package)_preprocess_cmds
+  patch -p1 < $($(package)_patch_dir)/include_ldflags_in_configure.patch
 endef
 
 define $(package)_config_cmds
@@ -22,3 +28,6 @@ define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
 
+define $(package)_postprocess_cmds
+  rm lib/*.la
+endef
