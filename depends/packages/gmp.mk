2799ec3b2e653ee614733a306b72f074c8cdd99b
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-15 12:33:25 -0500

    update

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -9,7 +9,7 @@ define $(package)_set_vars
 $(package)_config_opts += --disable-shared --enable-cxx --enable-fat
 $(package)_cflags_aarch64 += -march=armv8-a
 $(package)_cflags_armv7l += -march=armv7-a
-$(package)_cflags_x86_64 += -march=x86-64
+#$(package)_cflags_x86_64 += -march=x86-64
 endef
 
 define $(package)_preprocess_cmds
