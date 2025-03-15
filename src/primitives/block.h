70941af2fceee82a08978a3e66bba29d3d093ae0
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-15 10:26:12 -0500

    Update block.h

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -14,6 +14,12 @@
 
 extern uint32_t nPPSwitchTime;
 
+/**
+ * Return the currently selected parameters. This won't change after app
+ * startup, except for unit tests.
+ */
+const CChainParams &Params();
+
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
  * requirements.  When they solve the proof-of-work, they broadcast the block
