892e4d1a9f7b1579188fd4fbc84303554dc61938
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-15 00:20:18 -0500

    Update packages.mk

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -1,6 +1,6 @@
 packages:=boost libevent gmp bls-dash backtrace cmake
 
-qt_packages = qrencode zlib
+qt_packages = qrencode
 
 qt_linux_packages:=qt expat dbus libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon libxcb_util libxcb_util_render libxcb_util_keysyms libxcb_util_image libxcb_util_wm
 
