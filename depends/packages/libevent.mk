6173d4c3a28c23bbe66bd5478f8ce7961c29beba
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-16 04:04:37 -0500

    Update libevent.mk

--- depends/packages/libevent.mk
+++ depends/packages/libevent.mk
@@ -26,7 +26,7 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_config_cmds
-  $($(package)_cmake) -S .. -B .
+  $(host_prefix)/bin/cmake -S .. -B .
 endef
 
 define $(package)_build_cmds
