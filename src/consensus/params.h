4c014605a1a7fcb8256b2d1000e6aa4956732754
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-16 05:39:01 -0500

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -140,11 +140,18 @@ struct Params {
     LLMQType llmqTypePlatform{LLMQType::LLMQ_NONE};
     LLMQType llmqTypeMnhf{LLMQType::LLMQ_NONE};
 
-    Consensus::Params consensus2;
-	const Consensus::Params& GetConsensus2() const { return consensus2; }
-
 };
 } // namespace Consensus
 
 #define SCC_GEN_TIME 1601898626
 #endif // BITCOIN_CONSENSUS_PARAMS_H
+
+
+class CChainParm
+{
+
+	const Consensus::Params& GetConsensus2() const { return consensus2; }
+	
+	Consensus::Params consensus2;
+};
+
