ab54d1b9253063e83a886ba8d1044efc1c8c6c26
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-17 16:44:42 -0500

    Update block.h

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -14,6 +14,8 @@
 
 extern uint32_t nPPSwitchTime;
 
+class CChainParams;
+
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
  * requirements.  When they solve the proof-of-work, they broadcast the block
