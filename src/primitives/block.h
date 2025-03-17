5daabafdfe9e4c895e6c3dc209b02bf2d7b6225c
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-17 16:55:26 -0500

    Update block.h

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -14,8 +14,6 @@
 
 extern uint32_t nPPSwitchTime;
 
-class CChainParams;
-
 /** Nodes collect new transactions into a block, hash them into a hash tree,
  * and scan through nonce values to make the block's hash satisfy proof-of-work
  * requirements.  When they solve the proof-of-work, they broadcast the block
@@ -192,7 +190,7 @@ struct CBlockLocator
     }
 };
 
-const CChainParams &Params2();
+extern CChainParams &Params2();
 
 
 #endif // BITCOIN_PRIMITIVES_BLOCK_H
