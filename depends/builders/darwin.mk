d079e25fb2db823c6345978907e4423bf827b9ce
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-17 19:44:57 -0500

    Update darwin.mk

--- depends/builders/darwin.mk
+++ depends/builders/darwin.mk
@@ -16,7 +16,7 @@ build_darwin_DOWNLOAD=curl --location --fail --connect-timeout $(DOWNLOAD_CONNEC
 
 #darwin host on darwin builder. overrides darwin host preferences.
 darwin_CC=$(shell xcrun -f clang) -isysroot$(shell xcrun --show-sdk-path)
-darwin_CXX:=$(shell xcrun -f clang++) -stdlib=libc++ -fvisibility=hidden -isysroot$(shell xcrun --show-sdk-path)
+darwin_CXX:=$(shell xcrun -f clang++) -stdlib=libc++ -isysroot$(shell xcrun --show-sdk-path)
 darwin_AR:=$(shell xcrun -f ar)
 darwin_RANLIB:=$(shell xcrun -f ranlib)
 darwin_STRIP:=$(shell xcrun -f strip)
