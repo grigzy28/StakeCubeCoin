72770f87760e0843bb5e385f0fbc4737bbb9c9bc
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-17 19:31:23 -0500

    Update chainparams.h

--- src/chainparams.h
+++ src/chainparams.h
@@ -355,6 +355,7 @@ std::unique_ptr<const CChainParams> CreateChainParams(const std::string& chain);
  * Return the currently selected parameters. This won't change after app
  * startup, except for unit tests.
  */
+__attribute__((visibility("default"))
 const CChainParams &Params();
 
 /**
