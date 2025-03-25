4c081806757296d0a7f2eae43496c67481224baa
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-03-24 20:35:33 -0500

    Update miniupnpc.mk

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -12,7 +12,7 @@ define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
 $(package)_build_opts_darwin=LIBTOOL="$($(package)_libtool)"
 $(package)_build_env+=CFLAGS="$($(package)_cflags) $($(package)_cppflags)" AR="$($(package)_ar)"
-CMAKE=$($(package)_staging_prefix_dir)/bin/cmake
+CMAKE=$($(package)_staging_dir)/bin/cmake
 endef
 
 define $(package)_preprocess_cmds
