46567eedc083bec42a50470f518f47db67a81e25
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-08-31 15:31:15 -0500

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -2346,7 +2346,7 @@ bool AppInitMain(InitInterfaces& interfaces)
 
 
     auto fCleanupOldContributions = gArgs.GetBoolArg("-disablecleanup", false);
-    if (fCleanupOldContributions) {
+    if (!fCleanupOldContributions) {
 
         LogPrint(BCLog::ALL, "Enabled Cleanup of Old Contributions\n");
         // Convert std::chrono::hours to milliseconds
