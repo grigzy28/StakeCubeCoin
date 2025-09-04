7209240196976150e075fcef61951799c951caf4
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-09-04 10:08:27 -0500

    Update bls_worker.cpp

--- src/bls/bls_worker.cpp
+++ src/bls/bls_worker.cpp
@@ -63,9 +63,8 @@ CBLSWorker::~CBLSWorker()
 void CBLSWorker::Start()
 {
     int workerCount = std::thread::hardware_concurrency() / 2;
-    workerCount = std::max(std::min(1, workerCount), 1);
-    workerPool.resize(1);
-//    workerPool.resize(workerCount);
+    workerCount = std::max(std::min(1, workerCount), 4);
+    workerPool.resize(workerCount);
     RenameThreadPool(workerPool, "bls-work");
 }
 
