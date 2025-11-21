863016e30bd3d4d33ee56b5a8b373ccc23f62e2a
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-11-21 17:56:52 -0600

    Update versionbits.cpp

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -436,7 +436,7 @@ void VersionBitsCache::InitializeAsync(const CBlockIndex* tip,
 {
     if (preloadedchain.load()) return;
 
-    std::thread([this, tip, params] {
+    AbstractThresholdConditionChecker::vbworkerPool([this, tip, params](int) {
 //        RenameThread("vb-prefill");
 //        SetLowThreadPriority();
         SetBackgroundThreadPriority(); // cross‑platform helper already in Bitcoin Core util/
@@ -492,7 +492,7 @@ void VersionBitsCache::InitializeAsync(const CBlockIndex* tip,
 
         LogPrintf("Versionbits cache prefill complete.\n");
         preloadedchain.store(true);
-    }).detach();
+    });
 }
 
 /*
