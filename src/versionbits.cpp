864da102ffc4a36a72803d7a9ad233f2a0906305
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-11-21 07:58:05 -0600

    Update versionbits.cpp

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -447,9 +447,9 @@ void VersionBitsCache::InitializeAsync(const CBlockIndex* pindexPrev, const Cons
         caches[bit][pindex] = state = stateNext;
 //    }
 //}
-*/
 
             }
+*/
 //        LogPrintf("bit %d cache size: %zu entries\n", bit, caches[bit].size());
     for (unsigned int d = 0; d < Consensus::MAX_VERSION_BITS_DEPLOYMENTS; d++) {
         caches[d].clear();
