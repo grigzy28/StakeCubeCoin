4b03c2533f2387586faeccf539fc4caccd7e3a33
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-11-21 00:05:56 -0600

    Update versionbits.cpp

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -370,7 +370,7 @@ void VersionBitsCache::InitializeAsync(const CBlockIndex* pindexPrev, const Cons
 			      LogPrint(BCLog::BENCHMARK, "bit: %s build cache\n", bit);
 
 //            if (!preloadedchain && (preloadchaincounter < VERSIONBITS_NUM_BITS)) {
-  	        ThresholdState state = checker.GetStateForBuildCache(pindex, params, warningcache[bit], bit);
+//  	        ThresholdState state = checker.GetStateForBuildCache(pindex, params, warningcache[bit], bit);
         }
         
         for (int bit = 0; bit < Consensus::MAX_VERSION_BITS_DEPLOYMENTS; ++bit) {
@@ -391,7 +391,7 @@ void VersionBitsCache::InitializeAsync(const CBlockIndex* pindexPrev, const Cons
 			      LogPrint(BCLog::BENCHMARK, "bit: %s\n", bit);
 
 //            if (!preloadedchain && (preloadchaincounter < VERSIONBITS_NUM_BITS)) {
-//  	        ThresholdState state = checker.GetStateForBuildCache(pindex, params, warningcache[bit], bit);
+  	        ThresholdState state = checker.GetStateForBuildCache(pindex, params, warningcache[bit], bit);
 //            ThresholdState state = checker.GetStateFor(pindex, params, warningcache[bit]);
 
 			      LogPrint(BCLog::BENCHMARK, "bit: %s\n", bit);
