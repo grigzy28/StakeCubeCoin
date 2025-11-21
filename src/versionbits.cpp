e874f2e9aa5cc37a8df447f6b462b8c51901c38e
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-11-21 14:38:56 -0600

    Update versionbits.cpp

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -455,6 +455,8 @@ void VersionBitsCache::InitializeAsync(const CBlockIndex* tip,
         // Now, for each bit, process using that one snapshot
         for (int bit = 0; bit < Consensus::MAX_VERSION_BITS_DEPLOYMENTS; ++bit) {
 
+			      if (bit == 3) bit = 4;
+			        
 			      LogPrint(BCLog::BENCHMARK, "bit: %s build cache\n", bit);
 
             const auto& d = params.vDeployments[bit];
