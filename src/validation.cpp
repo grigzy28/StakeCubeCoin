b5e4e3422d62c18b1db27bb2320516fe908855b0
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-11-21 08:30:55 -0600

    Update validation.cpp

--- src/validation.cpp
+++ src/validation.cpp
@@ -2609,7 +2609,7 @@ void static UpdateTip(const CBlockIndex *pindexNew, const CChainParams& chainPar
 
                         WarningBitsConditionChecker checker(bit);
 */
-            ThresholdState state = checker.GetStateFor(pindex, chainParams.GetConsensus(), warningcache[bit]);
+            ThresholdState state = checker.GetStateFor(pindex, chainParams.GetConsensus(), caches[bit]);
             if (state == ThresholdState::ACTIVE || state == ThresholdState::LOCKED_IN) {
                 const std::string strWarning = strprintf(_("Warning: unknown new rules activated (versionbit %i)").translated, bit);
                 if (state == ThresholdState::ACTIVE) {
