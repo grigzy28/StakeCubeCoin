19e0b821f49316387dc87475920b71aa9b70e209
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-11-21 18:06:19 -0600

    Update versionbits.cpp

--- src/versionbits.cpp
+++ src/versionbits.cpp
@@ -447,7 +447,7 @@ void VersionBitsCache::InitializeAsync(const CBlockIndex* tip,
         std::vector<const CBlockIndex*> blocks;
         {
             LOCK(cs_main);
-            for (auto p = tip; p && blocks.size() < 144; p = p->pprev) {
+            for (auto p = tip; p && blocks.size(); p = p->pprev) {
                 blocks.push_back(p);
             }
         }
