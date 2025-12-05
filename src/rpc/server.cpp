e7df0d0711932ba0f74a9396db59a51c5527304c
grigzy28 <30666305+grigzy28@users.noreply.github.com>
2025-12-04 20:35:50 -0600

    Update server.cpp

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -578,13 +578,10 @@ void RPCRunLater(const std::string& name, std::function<void()> func, int64_t nS
     if (!timerInterface)
         throw JSONRPCError(RPC_INTERNAL_ERROR, "No timer handler registered for RPC");
 
-// LogPrintf("Before deadlineTimers function call\n");
-
     deadlineTimers.erase(name);
 
-// LogPrintf("After deadlineTimer function call\n");
+    LogPrint(BCLog::RPC, "queue run of timer %s in %i seconds (using %s)\n", name, nSeconds, timerInterface->Name());
 
-	LogPrint(BCLog::RPC, "queue run of timer %s in %i seconds (using %s)\n", name, nSeconds, timerInterface->Name());
     deadlineTimers.emplace(name, std::unique_ptr<RPCTimerBase>(timerInterface->NewTimer(func, nSeconds*1000)));
 }
 
